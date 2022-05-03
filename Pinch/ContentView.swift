9f530afcfa8309dd6ba1dd84bafe2a2d0a63f763
deepak <deepakkumardash313@gmail.com>
2022-05-03 20:57:49 +0530

    Initial Commit

---
+++ Pinch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Deepak kumar Dash on 03/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Deepak kumar Dash on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
