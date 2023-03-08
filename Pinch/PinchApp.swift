ec2829a52d76c2f14ef7f81724ada0bf678d3f5e
deepak <deepakkumardash313@gmail.com>
2023-03-08 19:41:52 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Deepak kumar Dash on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
